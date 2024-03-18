486f87ab39e5221449185e63a9da3c37b8f48509
Manish-shahi <zaynshahi619@gmail.com>
2024-03-18 18:13:56 +0545

    gemini cloned

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Sidebar from './component/sidebar/Sidebar'
+import Main from './component/main/Main'
+
+const App = () => {
+  return (
+    <>
+      <Sidebar></Sidebar>
+      <Main></Main>
+    </>
+  )
+}
+
+export default App
---
+++ src/component/main/Main.jsx
@@ -0,0 +1,84 @@
+import React, { useContext } from 'react'
+import './main.css'
+import { assets } from '../../assets/assets'
+import { Context } from '../../context/Context'
+
+const Main = () => {
+
+  const {onSent,recentPrompt,showResult,loading,resultData,setInput,input} = useContext(Context)
+
+  return (
+    <div className='main'>
+        <div className='nav'>
+            <p>Gemini</p>
+            <img src={assets.user_icon}/>
+        </div>
+        <div className='main-container'>
+
+          {!showResult
+          ?
+            <>
+              <div className='greet'>
+            <p><span>Hello, Dev.</span></p>
+            <p>How can I help you today?</p>
+          </div>
+          <div className='cards'>
+            <div className='card'>
+              <p>Suggest beautiful places to see on an upcoming road trip</p>
+              <img src={assets.compass_icon}/>
+            </div>
+            <div className='card'>
+              <p>Briefly summarize this concept: urban planning</p>
+              <img src={assets.bulb_icon}/>
+            </div>
+            <div className='card'>
+              <p>Brainstrom team bonding activities for our work retreat</p>
+              <img src={assets.message_icon}/>
+            </div>
+            <div className='card'>
+              <p>Improve the readability of the following code</p>
+              <img src={assets.code_icon}/>
+            </div>
+          </div>
+            </>
+        :
+        <div className='result'>
+            <div className='result-title'>
+              <img src={assets.user_icon}/>
+              <p>{recentPrompt}</p>
+            </div>
+            <div className='result-data'>
+              <img src={assets.gemini_icon}/>
+              {loading
+              ?<div className='loader'>
+                <hr />
+                <hr />
+                <hr />
+
+              </div>
+              :<p dangerouslySetInnerHTML={{__html:resultData}}></p>
+              }
+              
+            </div>
+        </div>
+        }
+          
+
+          <div className='main-bottom'>
+            <div className='search-box'>
+              <input onChange={(e)=>setInput(e.target.value)} value={input} type='text' placeholder='Enter a prompt here'></input>
+              <div>
+                <img src={assets.gallery_icon}/>
+                <img src={assets.mic_icon}/>
+                <img onClick={()=>onSent()} src={assets.send_icon}/>
+              </div>
+            </div>
+            <p className='bottom-info'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Facilis asperiores nihil modi assumenda quas ad, nisi odio. Molestias, veniam provident!</p>
+
+          </div>
+        </div>
+    </div>
+  )
+}
+
+export default Main
